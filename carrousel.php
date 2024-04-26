40b3c2df7dc1b8aef203be8b69af04736d90857d
Lenny Tran <lennytran2004@icloud.com>
2024-04-25 22:33:44 -0400

    Avancement du carrousel en classe

--- carrousel.php
+++ carrousel.php
@@ -1,18 +1,19 @@
 <?php
 /**
  * Plugin Name: Carrousel
- * Author: Lenny Tran
+ * Author: Eddy Martin
  * Description: Affiche le carrousel associé à une galerie de Wordpress
  * Version: 1.0.0
- * Plugin URI: https://github.com/LennyT2004
- * Author URI: https://referenced.ca/
+ * Plugin URI: https://github.com/eddytuto
+ * Author URI: https://referenced.ca
  */
 
- function enqueue_style_script() {
-   $version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
-   $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
 
-   wp_enqueue_style(   'em_plugin_carrousel_css',
+ function enqueue_style_script(){
+    $version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+    wp_enqueue_style(   'em_plugin_carrousel_css',
                      plugin_dir_url(__FILE__) . "style.css",
                      array(),
                      $version_css);
@@ -22,20 +23,27 @@
                     array(),
                     $version_js,
                     true);
+
  }
+// il faut  wp_head();  soit ajouté juste avant la balise </head>
+// et que wp_footer(); soit ajouté juste avant la balise </body>
+ add_action('wp_enqueue_scripts','enqueue_style_script');
 
- add_action('wp_enqueue_scripts', 'enqueue_style_script');
 
- function genere_html() {
-   $html = '
-   <button class="bouton_ouvrir">Ouvrir Carrousel</button>
-   <div class="carrousel">
-      <button href="" class="carrousel_x">X</button>
-      <figure class="carrousel_figure"></figure>
-      <form action="" class="carrousel_form"></form>
-   </div>
-   ';
 
-   return $html;
- }
+function genere_html(){
+
+    $html = '
+    <button class="bouton__ouvrir">Ouvrir Carrousel</button>
+    <div class="carrousel">
+        <button class="carrousel__x">X</button>
+        <figure class="carrousel__figure"></figure>
+        <form action="" class="carrousel__form"></form>
+    </div>
+    ';
+    return $html;
+}
+// [carrousel] juste après la galerie dans votre article ou page
+// Quand la fonction the_content() rencontrera [carrousel] c'est à ce moment 
+// que le carrousel sera initialisé
  add_shortcode('carrousel', 'genere_html');
